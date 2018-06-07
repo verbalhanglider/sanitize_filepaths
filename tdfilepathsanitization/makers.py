c579c79a1cdd7c66c0231be7adc503ab6cc0aca3
verbalhanglider <tdanstrom@uchicago.edu>
2018-06-07 16:27:57 -0500

    starting to get real code written finally!

--- tdfilepathsanitization/makers.py
+++ tdfilepathsanitization/makers.py
@@ -6,11 +6,11 @@ class FilePathBuilder:
         self.path = self._clean_path(some_path)
 
     def _clean_path(self, some_string):
-        some_string = whitelist_approved_chars(some_path_as_string):
         some_string = remove_unsafe_path_traversal_commands(some_string)  
+        some_string = whitelist_approved_chars(some_string)
         return some_string
 
-    def write(self, some_path):
-        some_string = create_unextant_parents(some_path_as_string)
-        some_string = create_directory(some_path_as_string)
- 
+    def write(self):
+        some_string = create_unextant_parents(self.path)
+        some_string = create_directory(self.path)
+        return some_string
--- tests/test_spec.py
+++ tests/test_spec.py
@@ -1,5 +1,36 @@
 
+from os.path import abspath
+from tempfile import TemporaryDirectory
 from unnittest import TestCase
 
+from tdffilepathsanitization.utils import *
+
 class SpecTests(TestCase):
-    def 
+    def setUp(self):
+        pass
+
+    def test_whitelist_approved_chars(self):
+        good_string = "/home/test/foo/bar/test/bomb"
+        new_string = whitelist_approve_chars(good_string)
+        self.assertEquals(good_string, new_string)
+
+    def test_remove_unsafe_path_traversal_commands(self):
+        bad_string = "/home/test/../../../var/log"
+        new_string = remove_unsafe_path_traversal_commands(bad_string)
+        self.assertEqual(new_string, '/home/test/var/log')
+
+    def test_are_the_parents_extant(self):
+        pass
+
+    def test_does_the_path_exist_already(self):
+        t = TemporaryDirectory()
+        new_path = abspath(Path(*[t.name, 'foo']).as_posix())
+        
+        pass
+
+    def test_create_unextant_parents(self):
+        pass
+
+    def test_create_directory(self):
+
+        pass
